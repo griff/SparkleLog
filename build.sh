57d80d83b500aad868f74229cd3df7eb8975f7e5
Brian Olsen <brian@maven-group.org>
2014-02-17 02:52:38 +0100

    Fixed build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 VERSION="$(git rev-parse --short HEAD)"
-docker build -t griff/sparklelog:$VERSION .
+docker build --rm -t griff/sparklelog:$VERSION .
 docker tag griff/sparklelog:$VERSION griff/sparklelog:latest
 
-docker run griff/xellia-profile:$VERSION tar zc -C app . > sparklelog.tar.gz
+docker run griff/sparklelog:$VERSION tar zc -C app . > sparklelog.tar.gz
